cbbd20f3cae80f3608412da07e0fe06fab945338
teresita cisneros estrada <cisneros0831@gmail.com>
2020-06-10 04:32:20 +0000

    Automatically backed up by Learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -32,12 +32,22 @@
         #your hashes here!
    },
    capulet:
-   patriarch: {name: "Lord Capulet", age: "50"},
-   matriarch: {name: "Lady Capulet", age: "51"},
-   heroine: {name: "Juliet", age: "15", status: "alive"},
+   patriarch: {
+   name: "Lord Capulet", 
+   age: "50"
+    },
+   matriarch: {
+   name: "Lady Capulet", 
+   age: "51"
+    },
+   heroine: {
+   name: "Juliet", 
+   age: "15", 
+   status: "alive"
+   },
    heroine_friends: [
     {name: "Steve", age: "30", attitude: "confused"},
-    {name: "Nurse", age "44", attitude: "worried"}
+    {name: "Nurse", age: "44", attitude: "worried"}
     ]  
    }
 }
