542f54aad18fbb1e9b43f484711c088f01d6c57e
teresita cisneros estrada <cisneros0831@gmail.com>
2020-06-10 04:10:55 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -1,9 +1,7 @@
 def third_challenge
   epic_tragedy = {
    :montague => {
-      :patriarch => {
-        #your key/value pairs here
-        },
+      :patriarch => {name: "Lord Montague", age "53"},
       :matriarch => {
         #your key/value pairs here
         },
