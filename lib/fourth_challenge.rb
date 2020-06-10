23f9a6637ab0940ceff14cdec10733aeb37da73c
teresita cisneros estrada <cisneros0831@gmail.com>
2020-06-10 04:28:48 +0000

    Automatically backed up by Learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -5,14 +5,23 @@
       :matriarch => {name: "Lady Montague", age: "54"},
       :hero => {name: "Romeo", age: "15", status: "alive"},
       :hero_friends => [
-        #your hashes here!
+       #your hashes here!
+      
       montague: {
-        patriarch: {name: "Lord Montague", age: "53"},
-        matriarch:{name: "Lord Montague", age: "54"},
-        hero: {name: "Romeo", age "15", status: "alive"},
+        patriarch: {
+        name: "Lord Montague", 
+        age: "53"},
+        matriarch: {name: "Lord Montague", 
+        age: "54"
+        },
+        hero: {
+        name: "Romeo", 
+        age "15", 
+        status: "alive"
+        },
         heroine_friends:[
-        {name;"Benvolio:,age:"17", attitude: "worried"},
-        {name "Mercutio", age "18",attitude: "hot-headed"}
+        {name:"Benvolio",age:"17", attitude: "worried"},
+        {name "Mercutio", age: "18", attitude: "hot-headed"}
       ]
    }, 
    :capulet => {
